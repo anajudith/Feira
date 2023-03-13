2bef1106124f8c5dfa142e9cf7069ab42d76d1f8
Ana Judith <ana-judth@hotmail.com>
2023-03-13 14:53:09 -0300

    Projeto iniciado com alguns componentes

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import Carrinho from 'pages/Carrinho';
+import Feira from 'pages/Feira';
+import Login from 'pages/Login';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { UsuarioProvider } from 'common/context/Usuario'
+import { CarrinhoProvider } from 'common/context/Carrinho';
+function Router() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <UsuarioProvider>
+                    <Route exact path="/">
+                        <Login />
+                    </Route>
+                    <CarrinhoProvider>
+                        <Route path="/feira">
+                            <Feira />
+                        </Route>
+                    </CarrinhoProvider>
+                </UsuarioProvider>
+                <Route exact path="/carrinho">
+                    <Carrinho />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Router;
